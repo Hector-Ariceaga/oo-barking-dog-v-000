c4700e03552d937469bac18a34f5182fbf4c34fd
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-04-12 22:36:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+  def name=(dog_name)
+    @name
+  end
+  
+  def name
+    @name
+  end
+  
+  def bark=(dog_bark)
+    @bark
+  end
+  
+  def bark
+    @bark
+  end
+end
